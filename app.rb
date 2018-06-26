70ad3fa8185318bb8f4b31d62fa059caae20b38f
Micah <micahhope@yahoo.com>
2018-06-26 22:05:28 +1000

    Sort boxes based on addresses and the distances between them

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require_relative 'lib/lib'
 
 c = Container.new(length: 5, width: 4, height: 6)
-b1 = Box.new(length: 2, width: 1, height: 3, receiver_address: '1')
-b2 = Box.new(length: 3, width: 2, height: 1, receiver_address: '1')
-b3 = Box.new(length: 1, width: 3, height: 2, receiver_address: '2')
+b1 = Box.new(length: 2, width: 1, height: 3, receiver_address: 'Ringwood, VIC, Australia')
+b2 = Box.new(length: 3, width: 2, height: 1, receiver_address: 'Belgrave, VIC, Australia')
+b3 = Box.new(length: 1, width: 3, height: 2, receiver_address: 'Boronia, VIC, Australia')
 
 c.add_box(b1, b2, b3)
+c.boxes.sort_by_starting_point('Melbourne, VIC, Australia')
--- lib/box_array.rb
+++ lib/box_array.rb
@@ -1,21 +1,84 @@
 require 'delegate'
+require 'faraday'
+require 'json'
 
 class BoxArray < SimpleDelegator
   
   def initialize(*elements)
     super([*elements])
-    @connection = Faraday.new(url: 'https://maps.googleapis.com/maps/api/distancematrix/json')
+    @connection = Faraday.new #(url: 'https://maps.googleapis.com/maps/api/distancematrix/json')
   end
   
   def sort_by_starting_point(address)
-    
+    build_request(address)
+    .yield_self { |params| make_request(params) }
+    .yield_self { |response| transform_response(response) }
+    .yield_self { |data| sort_by_distance(data) }
   end
   
-  private
-  attr_reader :connection
+  # private
+  attr_reader :connection, :address_pairs, :params, :response, :sender_address, :addresses
+  # \https://maps.googleapis.com/maps/api/distancematrix/json\?origins\=Vancouver+BC\|Seattle\&destinations\=San+Francisco\|Victoria+BC\&key\=
   
-  def make_calls
-    connection.get # ...
+  def build_request(address)
+    @sender_address = address
+    origins, destinations = build_addresses(@addresses = [address, *group_by_address.keys])
+    { origins: origins, destinations: destinations, key: ENV['MAPS_API'] }    
+  end
+  
+  def make_request(params)
+    response = connection.get 'https://maps.googleapis.com/maps/api/distancematrix/json', params
+    JSON.parse(response.body)
+  end
+  
+  def transform_response(response)
+    get_rows(response)
+    .yield_self { |rows| merge_addresses(rows) }
+    .yield_self { |data| remove_invalid_combinations(data)}
+  end
+  
+  def get_rows(response)
+    response["rows"].flat_map do |row|
+      row["elements"].map(&:itself)
+    end    
+  end
+  
+  def merge_addresses(rows)
+    rows.map.with_index do |run ,index|
+      address_pairs[index].merge(run)
+    end
+  end
+  
+  def remove_invalid_combinations(data)
+    data.reject { |c| c['origin'] == c['destination']}
+  end
+  
+  def build_addresses(addresses)
+    @address_pairs = addresses.flat_map do |a2|
+      addresses.map { |a3| { 'origin' => a2, 'destination' => a3 } }
+    end
+    addresse_list = addresses.map { |address| address.gsub(', ', '+')  }.join('|')
+    [addresse_list, addresse_list]
+  end
+  
+  def sort_by_distance(data)
+    data, leg = next_leg(data, sender_address)
+    addresses.each.with_index.reduce([leg]) do |trip, (_, index)|
+      if trip[index]
+        data, leg = next_leg(data, trip[index]['destination'])
+        trip << leg
+      else
+        trip
+      end
+    end
+  end
+  
+  def next_leg(data, address)
+    address_legs, new_data = data.partition { |a| a['origin'] == address || a['destination'] == address }
+    next_leg = address_legs.select { |a| a['origin'] == address }.min_by do |leg|
+      [leg['duration']['value'], leg['distance']['value']]
+    end
+    [new_data, next_leg]
   end
   
   def group_by_address
